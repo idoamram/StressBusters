9e31b750cad6d6d80094e1400fa67a126c7e1c06
idoamram <ido.movieditor@gmail.com>
2018-10-04 19:22:37 +0300

    1. added alert activity 2. re-added main activity in java with navigation drawer 3. modified the colors schema

---
+++ mobile/src/main/java/com/sb/stressbusters/AlertActivity.java
@@ -0,0 +1,13 @@
+package com.sb.stressbusters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert);
+    }
+}
